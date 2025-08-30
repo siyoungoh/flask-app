7a49d30e7982995c2b8a31299702a788db0ed380
SiyoungOh <81408668+siyoungoh@users.noreply.github.com>
2025-08-30 19:05:38 +0900

    chore: manage port via environment variable

--- app/app.py
+++ app/app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5002))
+    app.run(host="0.0.0.0", port=port, debug=True)
